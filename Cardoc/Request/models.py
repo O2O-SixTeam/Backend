f6820551ed13b210ee77dd5068d041e234daa02d
HYUN <changja00@gmail.com>
2017-04-03 22:33:27 +0900

    choice attr is add on broken fields

--- Cardoc/Request/models.py
+++ Cardoc/Request/models.py
@@ -8,6 +8,23 @@ EXTRA_CHOICE = (
     ('차대번호', '차대번호'),
 )
 
+BROKEN_CHOICE = (
+    ('범퍼', '범퍼'),
+    ('프론트 판넬', '프론트 판넬'),
+    ('앞 펜더', '앞 펜더'),
+    ('앞 휠하우스', '앞 휠하우스'),
+    ('에이필러', '에이필러'),
+    ('앞문', '앞문'),
+    ('비필러', '비필러'),
+    ('뒷문', '뒷문'),
+    ('뒤 펜더', '뒤 펜더'),
+    ('뒤 휠하우스', '뒤 휠하우스'),
+    ('루프판넬', '루프판넬'),
+    ('트렁크', '트렁크'),
+    ('리어 판넬', '리어 판넬'),
+    ('본네트', '본네트'),
+    ('유리', '유리'),
+)
 
 class Request(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='request', blank=False)
@@ -15,9 +32,9 @@ class Request(models.Model):
     brand = models.CharField(max_length=20, blank=True, null=True)
     model = models.CharField(max_length=20, blank=True, null=True)
     carnumber = models.CharField(max_length=20, blank=True, null=True)
-    broken1 = models.CharField(max_length=10, blank=True, null=True)
-    broken2 = models.CharField(max_length=10, blank=True, null=True)
-    broken3 = models.CharField(max_length=10, blank=True, null=True)
+    broken1 = models.CharField(choices=BROKEN_CHOICE, max_length=10, blank=True, null=True)
+    broken2 = models.CharField(choices=BROKEN_CHOICE, max_length=10, blank=True, null=True)
+    broken3 = models.CharField(choices=BROKEN_CHOICE, max_length=10, blank=True, null=True)
     photo1 = models.ImageField(max_length=None, blank=True, null=True)
     photo2 = models.ImageField(max_length=None, blank=True, null=True)
     photo3 = models.ImageField(max_length=None, blank=True, null=True)
