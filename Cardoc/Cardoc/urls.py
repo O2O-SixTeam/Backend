d4aca6177aad5e91aea215f767e95159d4a1c5b1
HYUN <changja00@gmail.com>
2017-04-11 19:36:08 +0900

    search is done

--- Cardoc/Cardoc/urls.py
+++ Cardoc/Cardoc/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.contrib.auth import admin
 from rest_framework.authtoken import views as authtoken_views
 from rest_framework.routers import DefaultRouter
 
@@ -32,6 +33,7 @@ router.register(r'estimate', estimateview.EstimateViewSet)
 
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^rest-auth/', include('rest_auth.urls')),
